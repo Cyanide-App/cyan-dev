fbf81d3f4787d2a7f2687c5aff398d65c89b0662
memecake788 <memecake788@gmail.com>
2024-09-04 00:50:49 +0000

    layout fix

--- cyan/src/App.jsx
+++ cyan/src/App.jsx
@@ -1,13 +1,14 @@
-
+import React from 'react';
 import './App.css'
-import { useState } from 'react';
 
-export default function Main(){
-  return(
-<div class="container">
-  <div class="Hero">test1</div>
-  <div class="Navbar">test2</div>
-  <div class="Animation">test3</div>
-</div>
-      );
+function App() {
+  return (
+<section class="layout">
+  <div class="header">1</div>
+  <div class="navbar">2</div>
+  <div class="animation">3</div>
+</section>
+  );
 }
+
+export default App;
