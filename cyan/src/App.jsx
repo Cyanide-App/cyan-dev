1e2182382b853a403b90563382aaf7025ce79976
memecake788 <memecake788@gmail.com>
2024-09-04 01:53:42 +0000

    fixed layout

--- cyan/src/App.jsx
+++ cyan/src/App.jsx
@@ -4,7 +4,10 @@ import './App.css'
 function App() {
   return (
 <section class="layout">
-  <div class="header">1</div>
+  <div class="header">
+    <h1 class="hero-text">cyλn</h1>
+    <h3 class="splash-text">temp</h3>
+  </div>
   <div class="navbar">2</div>
   <div class="animation">3</div>
 </section>
