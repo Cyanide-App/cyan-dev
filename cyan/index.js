72c39333a1a59b7bcb72f264d0a2563740879c63
memecake788 <memecake788@gmail.com>
2024-09-19 13:07:23 +0000

    vercel fix

--- cyan/index.js
+++ cyan/index.js
@@ -32,11 +32,11 @@ server.on('upgrade', (req, socket, head) => {
 })
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(process.cwd(), 'cyan/index.html'));
+    res.sendFile(path.join(process.cwd(), '/index.html'));
 });
 
 app.get('/index', (req, res) => {
-    res.sendFile(path.join(process.cwd(), 'cyan/index.html'));
+    res.sendFile(path.join(process.cwd(), '/index.html'));
 });
 
 /* add your own extra urls like this:
