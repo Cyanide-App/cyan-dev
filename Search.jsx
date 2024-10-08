4cfb4134a9a1c5d4dc28801964b9a71d1935cff8
Mem3 <132317002+MemeCake789@users.noreply.github.com>
2024-10-08 10:45:30 -0500

    dumping files

---
+++ Search.jsx
@@ -0,0 +1,8 @@
+import React, { useState, useEffect, useRef } from "react";
+import { HashRouter, Routes, Route, NavLink } from "react-router-dom";
+
+function Search() {
+  return <h1>search</h1>;
+}
+
+export default Search;
