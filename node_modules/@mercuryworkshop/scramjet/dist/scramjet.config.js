c565055912e8b8a01590204ea78a0672a83fd4d9
Mem3 Mathew <memecake788@gmail.com>
2024-10-08 14:03:42 +0000

    testing chemical js

---
+++ node_modules/@mercuryworkshop/scramjet/dist/scramjet.config.js
@@ -0,0 +1,1 @@
+(()=>{self.__scramjet$config={prefix:"/scramjet/",codec:self.__scramjet$codecs.plain,config:"/scramjet.config.js",bundle:"/scramjet.bundle.js",worker:"/scramjet.worker.js",client:"/scramjet.client.js",codecs:"/scramjet.codecs.js"};})();
